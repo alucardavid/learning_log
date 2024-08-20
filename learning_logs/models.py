c1aded6091a9f174bdcc471d6b2288f26ca2e64b
David Pereira <d_pereira@outlook.com.br>
2024-08-19 22:27:28 -0300

    creating home page

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -20,6 +20,11 @@ class Entry(models.Model):
 
     def __str__(self):
         """Return a simple string representing the entry."""
-        return f"{self.text[:50]}..."
+        
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        else:
+            return self.text
+            
     
 
